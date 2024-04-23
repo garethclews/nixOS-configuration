64d90237c2e2559ee8d7c36c07fee2584c112d66
karetsu <20670199+karetsu@users.noreply.github.com>
2024-04-23 15:52:29 +0100

    feat: home-manager added

--- flake.nix
+++ flake.nix
@@ -1,18 +1,24 @@
 {
-  description = "grunkleStanFlake";
+  description = "NixOS configuration";
 
   inputs = {
-    nixpkgs = {
-      url = "github:NixOS/nixpkgs/nixos-unstable";
-    };
+    nixpkg.url = "github:NixOS/nixpkgs/nixos-unstable";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
     nixosConfigurations = {
       grunkleStan = nixpkgs.lib.nixosSystem {
         system = "x86_64-linux";
         modules = [
           ./configuration.nix
+          home-manager.nixosModules.home-manager
+          {
+            home-manager.useGlobalPkgs = true;
+            home-manager.useUserPackages = true;
+            home-manager.users.aloysius = import ./home.nix;
+          }
         ];
       };
     };
